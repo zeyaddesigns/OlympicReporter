3ebb1d1f5405733e6621f74df6c92c602212fdad
ztaa1 <ztaa1@hw.ac.uk>
2021-11-27 00:36:58 +0000

    First commit

---
+++ src/Gamer.java
@@ -0,0 +1,21 @@
+
+public class Gamer extends Competitor
+{
+	//Instance variables
+	private String teamName;
+	private int prizeMoney;
+	
+	//Constructor
+	public Gamer (int id, Name name, String nation, String teamName, int prizeMoney)
+	{
+		super(id,name,nation);
+		this.teamName = teamName;
+		this.prizeMoney = prizeMoney;
+	}
+	
+	//Methods
+	public String getTeamName() { return teamName; }
+	public int getPrizeMoney() { return prizeMoney; }
+	public void setPrizeMoney(int prizeMoney) {this.prizeMoney = prizeMoney; }
+	public void setTeamName(String teamName) { this.teamName = teamName; }	
+}

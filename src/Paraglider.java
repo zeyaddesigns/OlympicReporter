34c467cb20949f03b7125cad6be807acd9653c62
ztaa1 <ztaa1@hw.ac.uk>
2021-11-30 10:08:01 +0000

    All classes setup

--- src/Paraglider.java
+++ src/Paraglider.java
@@ -1,4 +1,30 @@
 
-public class Paraglider {
-
+public class Paraglider extends Competitor
+{
+	//Instance variables
+	private double flyingSpeed;
+	private double altitude;
+	
+	//Constructor
+	public Paraglider(int id, Name name, String nation, String level, double flyingSpeed, double altitude) 
+	{
+		super(id, name, nation, level);
+		this.flyingSpeed = flyingSpeed;
+		this.altitude = altitude;
+	}
+	
+	//Methods
+	public double getFlyingSpeed() {return flyingSpeed;}
+	public double getAltitude() {return altitude;}
+	public void setAltitude(double altitude) {this.altitude = altitude;}
+	public void setFlyingSpeed(double flyingSpeed) {this.flyingSpeed = flyingSpeed;}
+	
+	public double getOverallScore() 
+	{
+		// calculation that takes into account
+		// near misses and run time to generate
+		// an overall score.
+		return 0;
+	}
+	
 }
--- src/Skateboarder.java
+++ src/Skateboarder.java
@@ -1,4 +1,26 @@
 
-public class Skateboarder {
-
+public class Skateboarder extends Competitor
+{
+	//Instance variables
+	private int numOfTricks;
+	
+	
+	//Constructor
+	public Skateboarder(int id, Name name, String nation, String level, int tricks)
+	{
+		super(id, name, nation, level);
+		numOfTricks = tricks;
+	}
+	
+	//Methods
+	public int getNumOfTricks() {return numOfTricks;}
+	public void setNumOfTricks(int numOfTricks) {this.numOfTricks = numOfTricks;}
+	
+	public double getOverallScore() 
+	{
+		// calculation that takes into account
+		// near misses and run time to generate
+		// an overall score.
+		return 0;
+	}
 }
